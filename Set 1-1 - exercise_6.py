6dbcf2b22242ceeccd286583ac3a7bf5596207dd
panosale <panosale@users.noreply.github.com>
2020-03-30 01:31:35 +0300

    Update Set 1-1 - exercise_6.py

--- Set 1-1 - exercise_6.py
+++ Set 1-1 - exercise_6.py
@@ -1,2 +1,5 @@
-light_year = 3e5*365*24*3.6e3
+light_year = 3e5 * 365 * 24 * 3.6e3
+sec_per_day = 24 * 3.6e3
+sec_per_year = sec_per_day * 365
 print(light_year,"km")
+print(sec_per_year,"sec/year")
