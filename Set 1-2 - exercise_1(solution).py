d302673674e21f265ded4c2e0d859072d97ab24b
panosale <62485609+panosale@users.noreply.github.com>
2020-04-02 15:29:04 +0300

    Add files via upload

---
+++ Set 1-2 - exercise_1(solution).py
@@ -0,0 +1,8 @@
+# exercise 3.1
+
+name = input("What is your name? ")
+age = input("What is your age? ")
+age = float(age)
+days = 365*age
+
+print('\n',name,'you are',days,'days old')
---
+++ Set 1-2 - exercise_1.py
@@ -0,0 +1,3 @@
+name = str(input("Enter your name: "))
+age = float(input("...and your age please: "))
+print(name , ', you are', age*365 , 'days old.')
