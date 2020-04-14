efbe3cc4b25e0d25cf2d2cc3f0cb14b7f4619b0c
panosale <62485609+panosale@users.noreply.github.com>
2020-04-14 15:26:25 +0300

    Update Set 1-2 - exercise_2.py

--- Set 1-2 - exercise_2.py
+++ Set 1-2 - exercise_2.py
@@ -3,4 +3,4 @@ theta = 0.5235987755982988 # float(input('Give theta (must be between 0 and π/2
 print(math.sin(theta))
 print(math.cos(theta))
 print(math.tan(theta))
-
+# Χρειάζεται έλεγχο γιατί τα αποτελέσματα δεν συμβαδίζουν με το παράδειγμα της άσκησης
