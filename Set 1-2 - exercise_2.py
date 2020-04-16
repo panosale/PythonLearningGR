7bcd11a8a9041d8d2cc27be7962d0015c0ab8cc5
panosale <62485609+panosale@users.noreply.github.com>
2020-04-16 14:46:35 +0300

    Update Set 1-2 - exercise_2.py

--- Set 1-2 - exercise_2.py
+++ Set 1-2 - exercise_2.py
@@ -1,6 +1,10 @@
 import math
-theta = 0.5235987755982988 # float(input('Give theta (must be between 0 and π/2) : '))
-print(math.sin(theta))
-print(math.cos(theta))
-print(math.tan(theta))
-# Χρειάζεται έλεγχο γιατί τα αποτελέσματα δεν συμβαδίζουν με το παράδειγμα της άσκησης
+# theta = 0.5235987755982988
+theta = float(input('Give theta (must be between 0 and π/2): '))
+if theta > 0 and theta < math.pi/2:
+    print('sin =', math.sin(theta))
+    print('cos =', math.cos(theta))
+    print('tan =', math.tan(theta))
+else:
+    print('theta is out of limits (0 to π/2).')
+# Χρειάζεται έλεγχο γιατί τα αποτελέσματα δεν συμβαδίζουν με το παράδειγμα της άσκησης (theta = 0.5235987755982988)
