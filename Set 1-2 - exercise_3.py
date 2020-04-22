50f3825ee7f319b9c8c052fdcb79c82b37e90125
panosale <panosale@users.noreply.github.com>
2020-04-22 18:01:55 +0300

    Μικρές αλλαγές
    Έγιναν κάποιες αλλαγές στον κώδικα.

--- Set 1-2 - exercise_3.py
+++ Set 1-2 - exercise_3.py
@@ -18,21 +18,21 @@ print('β) Last 5 characters     =', s[-5:])
 s2 = s[2:]
 s3 = ''
 for i in range(1, len(s2), 2):
-    s3 = s3 + s2[i]
+    s3 += s2[i]
 print('γ) Every 2nd character from "' + s2 + '" (starting from pos 3 of the initial string) =', s3)
 
 # *** δ) Print all characters starting from 2nd last towards 1st
 s2 = s[0:len(s)-1]
 s3 = ''
 for i in range(len(s2)-1, -1, -1):
-    s3 = s3 + s2[i]
+    s3 += s2[i]
 print('δ) Given string starting from 2nd last towards 1st =', s3)
 
 # *** ε) Print odd and even numbers separately
 s_odd = ''
 s_even = ''
 for i in range(0, len(s), 2):
-    s_odd = s_odd + s[i]
+    s_odd += s[i]
 for i in range(1, len(s), 2):
-    s_even = s_even + s[i]
+    s_even += s[i]
 print('ε) Letters from odd places are "' + s_odd + '" and from even places are "' + s_even + '".')
--- Set 1-2 - exercise_6.py
+++ Set 1-2 - exercise_6.py
@@ -10,6 +10,14 @@ def Min ( num1, num2, num3 ):
     if n3 < new_min:
         new_min = n3
     return new_min
+# Function that finds the minimum of the three given numbers
+def Max ( num1, num2, num3 ):
+    new_max = n1
+    if n2 > new_max:
+        new_max = n2
+    if n3 > new_max:
+        new_max = n3
+    return new_max
 # Ask for 3 numbers
 n1 = int ( input ( 'Give 1st number: ' ) )
 n2 = int ( input ( 'Give 2nd number: ' ) )
@@ -19,3 +27,5 @@ print ( '**********' )
 print ( 'The average of the given numbers is :', Avg ( n1, n2, n3 ))
 # Calls the Min function with the 3 numbers
 print ( 'The minimum of the given numbers is :', Min ( n1, n2, n3 ))
+# Calls the Max function with the 3 numbers
+print ( 'The maximum of the given numbers is :', Max ( n1, n2, n3 ))
