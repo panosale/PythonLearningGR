fb8ca2493d69be8e4e4683588f878a6e9b1c4500
panosale <62485609+panosale@users.noreply.github.com>
2020-04-24 15:04:23 +0300

    Add files via upload

---
+++ Set 1-3 - exercise_1.py
@@ -0,0 +1,9 @@
+print('----- Set 1-3 - excercise_1 -----')
+# Τι θα τυπώσουν οι παρακάτω εντολές;
+n = 35
+if n % 5 == 0:
+    print('The number is divisible by 5')
+elif n % 7 == 0:
+    print('The number is divisible by 7')
+else:
+    print('I have no idea...')
