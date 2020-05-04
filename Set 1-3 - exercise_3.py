964ff4079183f2e3fed6a8599675d14a483b29ee
panosale <62485609+panosale@users.noreply.github.com>
2020-05-04 23:43:17 +0300

    Update Set 1-3 - exercise_3.py

--- Set 1-3 - exercise_3.py
+++ Set 1-3 - exercise_3.py
@@ -1,15 +1,13 @@
 print ( '----- Set 1-3 - excercise_3 -----' )
 # 3. Γράψτε ένα πρόγραμμα Python το οποίο διαβάζει ένα πραγματικό αριθμό x
-# και υπολογίζει την τιμή συνάρτησης (δίνεται στο .pdf των ασκήσεων.
+# και υπολογίζει την τιμή συνάρτησης (δίνεται στο .pdf των ασκήσεων).
 import math
 def funcF ( x ):
     if x <= 0:
         return ( 3 * ( x ** 2 ) ) + 1
-    elif x > 0 and x <= 3:
+    elif x <= 3 and x > 0:
         return 2 * x + 1
     else:
         return math.sqrt ( 46 + x )
-        
 x = int ( input ( 'Give x: ') )
-print ( 'x =', x )
-print ( 'f(x) =', funcF ( x ) )
+print ( 'f('+str(x)+') =', funcF ( x ) )
