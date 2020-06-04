3abc8edbc52da25077405042bdf568e9c8c8b485
panosale <panosale@users.noreply.github.com>
2020-06-05 00:25:22 +0300

    Exercise 3 corrections
    Έγιναν διορθώσεις στην άσκηση 3.

--- Set 1-4 - exercise_3.py
+++ Set 1-4 - exercise_3.py
@@ -5,6 +5,11 @@ print ( '----- Set 1-4 - excercise_3 -----' )
 # ...
 # 9  18  27 ...81
 
-numbers_list = [1, 2, 3, 4, 6, 7, 8, 9], [1, 2, 3, 4, 6, 7, 8, 9]
-
-print ( numbers_list )
+n_lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]#, [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i in range (0, len (n_lst) ):
+    print (n_lst)
+    n_lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for j in range (0, len (n_lst) ):
+        n_lst[j] = n_lst[j] * (i + 1)
+    #print (i, ' = ', (i) * n_lst[i])
+#print ( n_lst )
