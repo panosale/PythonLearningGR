020c6b6b378663cab17c0d47e4a273c7c7a48456
panosale <62485609+panosale@users.noreply.github.com>
2020-06-22 17:50:03 +0300

    Update Set 1-4 - exercise_5.py

--- Set 1-4 - exercise_5.py
+++ Set 1-4 - exercise_5.py
@@ -4,12 +4,12 @@ print ( '----- Set 1-4 - excercise_5 -----' )
 # Υπενθυμίζουμε ότι οι όροι της ακολουθίας Fibonacci ορίζονται ως εξής:
 # F0=0, F1=1 και Fn=Fn-1+Fn-2 για Fn>=2
 n = int(input('Give n: '))
-f1, f2, f = 0, 0, 0
+f, f1, f2 = 0, 0, 0
 for i in range(0, n):
     if i < 2:
         f = i
     else:
-        f  = f1 + (i - 2)
-    print(i, i-1, i-2, '=', f , f1)
-    f1 = f
-print(n)
+        f  = f1 + f2
+    f1 = f2
+    f2 = f
+    print(f)
