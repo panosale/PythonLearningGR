1073f803b08f28693f2de2697b7a2dcffaf4ceae
panosale <panosale@users.noreply.github.com>
2021-03-01 23:22:27 +0200

    GR-Emvoliasmoi completed

--- Exercises/GR-Emvoliasmoi.py
+++ Exercises/GR-Emvoliasmoi.py
@@ -1,7 +1,7 @@
 import requests
 import datetime
 #import json
-url = 'https://data.gov.gr/api/v1/query/mdg_emvolio?date_from=2020-12-28&date_to=2021-02-28'
+url = 'https://data.gov.gr/api/v1/query/mdg_emvolio'#?date_from=2020-12-28&date_to=2021-02-28'
 headers = {'Authorization':'Token afff74366084109b050512ce13d4b11c24bd3396'}
 response = requests.get(url, headers=headers)
 #with open('GR-Emvoliasmoi.txt', 'w', encoding='utf-8') as f:
@@ -9,13 +9,11 @@ response = requests.get(url, headers=headers)
 j = response.json()
 j = sorted(j, key= lambda k: k['referencedate'], reverse=False) # Ascending sort by 'referencedate'
 nomos = str(input("Δώστε νομό (Ελληνικά κεφαλαία π.χ. ΘΕΣΣΑΛΟΝΙΚΗΣ): "))
-date_from = str(input("Από... (ΕΕΕΕ-ΜΜ-ΗΗ): "))
-date_to = str(input("έως... (ΕΕΕΕ-ΜΜ-ΗΗ): "))
-d_from = datetime.datetime.strptime(date_from,'%Y-%m-%d')
-d_to = datetime.datetime.strptime(date_to,'%Y-%m-%d')
+date_from = datetime.datetime.strptime(str(input("Από... (ΕΕΕΕ-ΜΜ-ΗΗ): ")),'%Y-%m-%d')
+date_to = datetime.datetime.strptime(str(input("έως... (ΕΕΕΕ-ΜΜ-ΗΗ): ")),'%Y-%m-%d')
 for p in j:
     date_time_obj = datetime.datetime.strptime(p['referencedate'], '%Y-%m-%dT%H:%M:%S')
-    if nomos == p['area'] and (date_time_obj.date > d_from or date_time_obj.date < d_to): #Θέλει διόρθωση
+    if nomos == p['area'] and (date_time_obj.date() >= date_from.date() and date_time_obj.date() <= date_to.date()):
         print('Area:', p['area'],
               ' - Date=', date_time_obj.date(),
               ' - DayTotal=', p['daytotal'],
