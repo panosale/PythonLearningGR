06f164760d7c93dbe9d235a5ee0ac1afd1a6faad
panosale <panosalevr@hotmail.gr>
2021-04-10 16:53:53 +0300

    Update GetActiveWindowName.py

--- MyPrograms/GetActiveWindowName.py
+++ MyPrograms/GetActiveWindowName.py
@@ -1,10 +1,8 @@
 import win32gui
-i = 1
 w = win32gui
 last_window = w.GetWindowText (w.GetForegroundWindow())
-print (i, last_window)
-while i < 10000000:
+print (last_window)
+while True:
     if last_window != w.GetWindowText(w.GetForegroundWindow()):
-        print (i, w.GetWindowText(w.GetForegroundWindow()))
+        print (w.GetWindowText(w.GetForegroundWindow()))
         last_window = w.GetWindowText (w.GetForegroundWindow())
-    i += 1
