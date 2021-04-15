46402f43900f1a478a7c863875323e24cbc281b8
panosale <62485609+panosale@users.noreply.github.com>
2021-04-15 15:13:37 +0300

    Update MeasureMemoryUsage1.py

--- MyPrograms/MeasureMemoryUsage1.py
+++ MyPrograms/MeasureMemoryUsage1.py
@@ -1,1 +1,11 @@
+# Μέτρηση χρήσης μνήμης κατά την εκτέλεση ενός προγράμματος python με τη χρήση της βιβλιοθήκης resource.
+# 1ος τρόπος με περισσότερη κατανάλωση πόρων.
+# Από: https://medium.com/survata-engineering-blog/monitoring-memory-usage-of-a-running-python-program-49f027e3d1ba
 
+import tracemalloc
+
+tracemalloc.start()
+my_complex_analysis_method()  # <--- ΤΟ ΟΝΟΜΑ ΤΗΣ ΣΥΝΑΡΤΗΣΗΣ ΜΑΣ
+current, peak = tracemalloc.get_traced_memory()
+print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
+tracemalloc.stop()
