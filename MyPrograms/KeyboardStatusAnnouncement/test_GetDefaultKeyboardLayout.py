f764341bfd177fbd63174ec25691c7719a15b577
panosale <panosalevr@hotmail.gr>
2021-05-27 00:20:57 +0300

    Update test_GetDefaultKeyboardLayout.py

--- MyPrograms/KeyboardStatusAnnouncement/test_GetDefaultKeyboardLayout.py
+++ MyPrograms/KeyboardStatusAnnouncement/test_GetDefaultKeyboardLayout.py
@@ -1,3 +1,16 @@
 import ctypes
+import py_win_keyboard_layout
 user32 = ctypes.WinDLL('user32', use_last_error=True)
-print (user32.GetForegroundWindow())
+handle = user32.GetForegroundWindow()
+threadid = user32.GetWindowThreadProcessId(handle, 0)   # Get the thread id from that window handle
+layout_id = user32.GetKeyboardLayout(threadid)          # Get the keyboard layout id from the threadid
+
+#kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+#def_layout_id = kernel32.GetSystemDefaultUILanguage()
+#def_language_id = def_layout_id & (2 ** 16 - 1)
+#language_id_hex = hex(def_language_id)
+
+print (handle)
+print (threadid)
+#print (def_layout_id)
+#print (language_id_hex)
