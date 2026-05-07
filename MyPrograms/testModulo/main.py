030661248417f6d4e4ab74cf93a8f82b05892349
panosale <panosalevr@hotmail.gr>
2026-05-07 22:47:57 +0300

    .

--- MyPrograms/testModulo/main.py
+++ MyPrograms/testModulo/main.py
@@ -1,21 +1,23 @@
 def is_sorted(lst):
     lst = sorted(lst)
-    for i in range(len(lst) -1):
-        if lst[i+1] - lst[i] != 1:
+    for i in range(len(lst) - 1):
+        if lst[i + 1] - lst[i] != 1:
             return False
     return True
+
 
 modulo = int(input("Δώσε το modulo: "))
 prim_root = int(input("Δώσε την υποψήφια Πρωτογενή Ρίζα: "))
 mod_arr = []
 mod_arr.clear()
 for i in range(1, modulo):
-#    pwr = pow(prim_root, i)
-#    mod = pwr % modulo
+    # pwr = pow(prim_root, i)
+    # mod = pwr % modulo
     mod_arr.append(pow(prim_root, i) % modulo)
-if is_sorted(mod_arr):
-    print('Το ', prim_root, ' ΕΙΝΑΙ Πρωτογενής Ρίζα του ', modulo, '.', sep="")
-else:
-    print('Το ', prim_root, ' ΔΕΝ ΕΙΝΑΙ Πρωτογενής Ρίζα του ', modulo, '.', sep="")
-
+print("Για να είναι το " , prim_root , " Πρωτογενής Ρίζα του " , modulo , " πρέπει ο παρακάτω πίνακας να περιέχει όλους τους αριθμούς από το 1 έως το " , modulo - 1 , " (δηλ. modulo-1).", sep="")
 print(sorted(mod_arr))
+if is_sorted(mod_arr):
+    print("Οπότε, το ", prim_root, " ΕΙΝΑΙ Πρωτογενής Ρίζα του ", modulo, ".", sep="")
+else:
+    print("Οπότε, το ", prim_root, " ΔΕΝ ΕΙΝΑΙ Πρωτογενής Ρίζα του ", modulo, ".", sep="")
+
